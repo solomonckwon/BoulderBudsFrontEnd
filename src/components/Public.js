ebf1585670a2446e0b5e0d48668262a0a907db0a
solomonckwon <49326267+solomonckwon@users.noreply.github.com>
2024-08-12 22:55:43 -0700

    formatting

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,15 +7,15 @@ const Public = () => {
                 <h1>Welcome to <span className="nowrap">BoulderBuds!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>For a friendly competition to encourage competitive spirit of climbers.</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                    Gumby Gary<br />
+                    Buttermilk Road<br />
+                    Bishop, CA 93514<br />
+                    {/* <a href="tel:+15555555555">(555) 555-5555</a> */}
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                {/* <p>Owner: Dan Davidson</p> */}
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>

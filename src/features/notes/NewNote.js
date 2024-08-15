81d4656aa16c6089c25d7c0c06dd52cde239c61a
solomonckwon <49326267+solomonckwon@users.noreply.github.com>
2024-08-15 12:35:08 -0700

    Added login page

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -5,6 +5,8 @@ import NewNoteForm from './NewNoteForm'
 const NewNote = () => {
     const users = useSelector(selectAllUsers)
 
+    if (!users?.length) return <p>Not Currently Available</p>
+
     const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
 
     return content

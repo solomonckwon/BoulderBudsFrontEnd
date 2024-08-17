8aea3195800d6d56b504dc740088545042046224
solomonckwon <49326267+solomonckwon@users.noreply.github.com>
2024-08-17 13:37:40 -0700

    Getting code ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>

94526d4283d684afe6141f0f7381fd03cd79a11b
Solomon Kwon <solomonckwon@gmail.com>
2024-09-12 15:19:05 -0700

    Dash sidebar replaces header and footer

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,56 +1,34 @@
-import { Link } from 'react-router-dom'
-import useAuth from '../../hooks/useAuth'
-import ClimbsList from '../climbs/ClimbsList'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import useAuth from '../../hooks/useAuth';
+import ClimbsList from '../climbs/ClimbsList';
+import NotesList from '../notes/NotesList';
+import Box from '@mui/material/Box';
 
 const Welcome = () => {
-    const { username, isAdmin } = useAuth()
+    const { username } = useAuth();
 
-    const date = new Date()
-    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+    const date = new Date();
+    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date);
 
     const content = (
-        <section className="welcome">
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, minHeight: '95vh' }}>
+            <Box sx={{ flex: 1 }}>
+                <h1>Welcome {username}!</h1>
+    
+                <h3>Climbs</h3>
+                <ClimbsList />
+    
+                <h3>Notes</h3>
+                <NotesList />
+            </Box>
 
-            <p>{today}</p>
+            <Typography sx={{ alignSelf: 'flex-end', p: 1 }}>
+                {today}
+            </Typography>
+        </Box>
+    );
 
-            <h1>Welcome {username}!</h1>
-
-            <ClimbsList />
-
-            <Stack spacing={2} direction="row">
-            <Button variant='contained'>
-                    <Link to="/dash/climbs">View Climbs</Link>
-                </Button>
-                <Button variant='contained'>
-                    <Link to="/dash/climbs/new">Add New Climb</Link>
-                </Button>
-            </Stack>
-            
-            <Stack spacing={2} direction="row">
-                <Button variant='contained'>
-                    <Link to="/dash/notes">View Notes</Link>
-                </Button>
-                <Button variant='contained'>
-                    <Link to="/dash/notes/new">Add New Note</Link>
-                </Button>
-            </Stack>
-            
-            {(isAdmin) &&
-                <Stack spacing={2} direction="row">
-                    <Button variant='contained'>
-                        <Link to="/dash/users">View User Settings</Link>
-                    </Button>
-                    <Button variant='contained'>
-                        <Link to="/dash/users/new">Add New User</Link>
-                    </Button>
-                </Stack>
-            }
-
-        </section>
-    )
-
-    return content
+    return content;
 }
-export default Welcome
+
+export default Welcome;
